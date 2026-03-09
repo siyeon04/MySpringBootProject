d735c7f5e037ca6dde5a95a8bc8a80e0f5f92b34
siyeon04 <seanna0417@gmail.com>
2026-03-09 13:35:25 +0900

    file add

--- myspringboot/src/main/java/com/basic/myspringboot/MySpringBootApplication.java
+++ myspringboot/src/main/java/com/basic/myspringboot/MySpringBootApplication.java
@@ -1,19 +1,26 @@
 package com.basic.myspringboot;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class MySpringBootApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MySpringBootApplication.class, args);
-	}
+    public static void main(String[] args) {
+
+//        SpringApplication.run(MySpringBootApplication.class, args);
+        SpringApplication application = new SpringApplication(MySpringBootApplication.class);
+        //어플리케이션 타입 설정하기
+        application.setWebApplicationType(WebApplicationType.SERVLET);
+        application.run(args);
+
+    }
 
     @Bean
-    public String hello(){
-        System.out.println("=====Spring Bean 입니다.");
+    public String hello() {
+        System.out.println("=====Spring Bean 입니다. ====");
         return "Hello Bean";
     }
 }
--- myspringboot/src/main/java/com/basic/myspringboot/runner/MyRunner.java
+++ myspringboot/src/main/java/com/basic/myspringboot/runner/MyRunner.java
@@ -1,4 +1,72 @@
 package com.basic.myspringboot.runner;
 
-public class MyRunner {
+import com.basic.myspringboot.config.CustomerVO;
+import com.basic.myspringboot.property.MyBootProperties;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
+
+import java.util.function.Consumer;
+
+@Component
+public class MyRunner implements ApplicationRunner {
+    @Value("${myboot.name}")
+    private String name;
+
+    @Value("${myboot.age}")
+    private int age;
+
+    @Autowired
+    private Environment environment;
+
+    @Autowired
+    private MyBootProperties properties;
+
+    @Autowired
+    private CustomerVO customerVO;
+
+    //Logger 객체생성
+    private Logger logger = LoggerFactory.getLogger(MyRunner.class);
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        System.out.println("Logger 구현체 클래스명 = " + logger.getClass().getName());
+        logger.info("현재 활성화된 CustomerVO = {}", customerVO);
+        logger.info("MyBootProperties getName() = {}", properties.getName());
+        logger.info("MyBootProperties getFullName() = {}", properties.getFullName());
+
+        logger.info("${myboot.name}  = {}", name);
+        logger.info("${myboot.age}  = {}", age);
+
+        logger.debug("DEBUG 레벨");
+        logger.debug(">>> ${myboot.fullName}  = {}", environment.getProperty("myboot.fullName"));
+        logger.debug("VM 아규먼트 foo : {}", args.containsOption("foo"));
+        logger.debug("Program 아규먼트 bar : {}", args.containsOption("bar"));
+
+        /*
+            default void forEach(Consumer<? super T> action)
+            Consumer 인터페이스의 void accept(T t)
+         */
+        //Anonymous Inner Class
+        args.getOptionNames()
+                .forEach(new Consumer<String>() {
+                    @Override
+                    public void accept(String s) {
+                        System.out.println(s);
+                    }
+                });
+
+        //Argument 목록 출력하기 (람다식)
+        args.getOptionNames()//Set<String>
+                .forEach(name -> System.out.println(name));
+
+        //Methoer Reference (메서드 레퍼런스)
+        args.getOptionNames()//Set<String>
+                .forEach(System.out::println);
+    }
 }
